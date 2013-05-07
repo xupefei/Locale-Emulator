1b0a49770f891f2831a4c8368ea9af3c9dd505fa
Paddy Xu <xupaddy@gmail.com>
2013-05-07 15:22:20 +0800

    Update lang files

--- LEGUI/Properties/AssemblyInfo.cs
+++ LEGUI/Properties/AssemblyInfo.cs
@@ -50,4 +50,4 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.5.0")]
+[assembly: AssemblyVersion("1.0.6.0")]
