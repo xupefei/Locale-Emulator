a43f0d835ee1511f8d08b22372becba6637be0a1
Paddy Xu <xupaddy@gmail.com>
2013-05-07 11:56:14 +0800

    Error handling while determining PEType

--- LECommonLibrary/PEFileReader.cs
+++ LECommonLibrary/PEFileReader.cs
@@ -16,26 +16,33 @@ namespace LECommonLibrary
             if (string.IsNullOrEmpty(path))
                 return PEType.Unknown;
 
-            var br =
-                new BinaryReader(new FileStream(path,
-                                                FileMode.Open,
-                                                FileAccess.Read,
-                                                FileShare.ReadWrite
-                                     ));
+            try
+            {
+                var br =
+                    new BinaryReader(new FileStream(path,
+                                                    FileMode.Open,
+                                                    FileAccess.Read,
+                                                    FileShare.ReadWrite
+                                         ));
 
-            br.BaseStream.Seek(0x3C, SeekOrigin.Begin);
-            br.BaseStream.Seek(br.ReadInt32() + 4, SeekOrigin.Begin);
-            ushort machine = br.ReadUInt16();
+                br.BaseStream.Seek(0x3C, SeekOrigin.Begin);
+                br.BaseStream.Seek(br.ReadInt32() + 4, SeekOrigin.Begin);
+                ushort machine = br.ReadUInt16();
 
-            br.Close();
+                br.Close();
 
-            if (machine == 0x014C)
-                return PEType.X32;
+                if (machine == 0x014C)
+                    return PEType.X32;
 
-            if (machine == 0x8664)
-                return PEType.X64;
+                if (machine == 0x8664)
+                    return PEType.X64;
 
-            return PEType.Unknown;
+                return PEType.Unknown;
+            }
+            catch
+            {
+                return PEType.Unknown;
+            }
         }
     }
 }
