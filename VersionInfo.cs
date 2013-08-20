76ee15fc03b240e3e45e6d5092871e9d5df29131
Paddy Xu <xupaddy@gmail.com>
2013-08-20 14:34:36 +0800

    Update version number to 1.0.7.2

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -12,4 +12,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.7.1")]
+[assembly: AssemblyVersion("1.0.7.2")]
