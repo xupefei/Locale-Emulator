950f9569460b18b9cc2607ce48c3559824873212
Paddy Xu <xupaddy@gmail.com>
2014-01-19 15:28:59 +0100

    Implement update checker

--- LEInstaller/Form1.Designer.cs
+++ LEInstaller/Form1.Designer.cs
@@ -30,8 +30,7 @@
         {
             this.buttonInstall = new System.Windows.Forms.Button();
             this.buttonUninstall = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.buttonUpgrade = new System.Windows.Forms.Button();
+            this.labelVersion = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonInstall
@@ -39,7 +38,7 @@
             this.buttonInstall.Location = new System.Drawing.Point(16, 15);
             this.buttonInstall.Margin = new System.Windows.Forms.Padding(4);
             this.buttonInstall.Name = "buttonInstall";
-            this.buttonInstall.Size = new System.Drawing.Size(137, 69);
+            this.buttonInstall.Size = new System.Drawing.Size(246, 69);
             this.buttonInstall.TabIndex = 0;
             this.buttonInstall.Text = "Install";
             this.buttonInstall.UseVisualStyleBackColor = true;
@@ -47,41 +46,31 @@
             // 
             // buttonUninstall
             // 
-            this.buttonUninstall.Location = new System.Drawing.Point(306, 15);
+            this.buttonUninstall.Location = new System.Drawing.Point(270, 15);
             this.buttonUninstall.Margin = new System.Windows.Forms.Padding(4);
             this.buttonUninstall.Name = "buttonUninstall";
-            this.buttonUninstall.Size = new System.Drawing.Size(137, 69);
+            this.buttonUninstall.Size = new System.Drawing.Size(246, 69);
             this.buttonUninstall.TabIndex = 1;
             this.buttonUninstall.Text = "Uninstall";
             this.buttonUninstall.UseVisualStyleBackColor = true;
             this.buttonUninstall.Click += new System.EventHandler(this.buttonUninstall_Click);
             // 
-            // label1
+            // labelVersion
             // 
-            this.label1.Location = new System.Drawing.Point(308, 88);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(137, 17);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Version 0.0.0.0";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            // 
-            // buttonUpgrade
-            // 
-            this.buttonUpgrade.Location = new System.Drawing.Point(161, 15);
-            this.buttonUpgrade.Margin = new System.Windows.Forms.Padding(4);
-            this.buttonUpgrade.Name = "buttonUpgrade";
-            this.buttonUpgrade.Size = new System.Drawing.Size(137, 69);
-            this.buttonUpgrade.TabIndex = 3;
-            this.buttonUpgrade.Text = "Upgrade";
-            this.buttonUpgrade.UseVisualStyleBackColor = true;
+            this.labelVersion.AutoSize = true;
+            this.labelVersion.Location = new System.Drawing.Point(12, 88);
+            this.labelVersion.Name = "labelVersion";
+            this.labelVersion.Size = new System.Drawing.Size(161, 17);
+            this.labelVersion.TabIndex = 3;
+            this.labelVersion.Text = "Checking new version ...";
+            this.labelVersion.Click += new System.EventHandler(this.labelVersion_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(457, 114);
-            this.Controls.Add(this.buttonUpgrade);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(530, 115);
+            this.Controls.Add(this.labelVersion);
             this.Controls.Add(this.buttonUninstall);
             this.Controls.Add(this.buttonInstall);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -94,6 +83,7 @@
             this.TopMost = true;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -101,8 +91,7 @@
 
         private System.Windows.Forms.Button buttonInstall;
         private System.Windows.Forms.Button buttonUninstall;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button buttonUpgrade;
+        private System.Windows.Forms.Label labelVersion;
     }
 }
 
