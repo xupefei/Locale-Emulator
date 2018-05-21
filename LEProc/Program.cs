0bb4a81091d294166aa7a29955378b0e5aa38f8b
Paddy Xu <xupaddy@gmail.com>
2018-05-21 19:46:49 +0300

    Update Program.cs

--- LEProc/Program.cs
+++ LEProc/Program.cs
@@ -77,10 +77,6 @@ namespace LEProc
                     "-global\tOpen Global Profile Manager.\r\n" +
                     "\r\n" +
                     "\r\n" +
-                    "Have a suggestion? Want to report a bug? You're welcome! \r\n" +
-                    "Go to https://github.com/xupefei/Locale-Emulator/issues.\r\n" +
-                    "\r\n" +
-                    "\r\n" +
                     "You can press CTRL+C to copy this message to your clipboard.\r\n",
                     "Locale Emulator Version " + GlobalHelper.GetLEVersion()
                 );
