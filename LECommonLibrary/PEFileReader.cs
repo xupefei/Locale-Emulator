eae640ee8a816999d0f993b636baa1ea21664e2f
Paddy Xu <xupaddy@gmail.com>
2018-09-15 19:02:02 +0300

    Fix locking file when PEType failed. Origianl work by @shangjiaxuan.

--- LECommonLibrary/PEFileReader.cs
+++ LECommonLibrary/PEFileReader.cs
@@ -18,26 +18,30 @@ namespace LECommonLibrary
 
             try
             {
-                var br =
-                    new BinaryReader(new FileStream(path,
+                using (var br = new BinaryReader(new FileStream(path,
                                                     FileMode.Open,
                                                     FileAccess.Read,
-                                                    FileShare.ReadWrite
-                                         ));
+                                                    FileShare.ReadWrite)))
+                {
+                    if (br.BaseStream.Length < 0x3C + 4 || br.ReadUInt16() != 0x5A4D)
+                        return PEType.Unknown;
 
-                br.BaseStream.Seek(0x3C, SeekOrigin.Begin);
-                br.BaseStream.Seek(br.ReadInt32() + 4, SeekOrigin.Begin);
-                var machine = br.ReadUInt16();
+                    br.BaseStream.Seek(0x3C, SeekOrigin.Begin);
+                    var pos = br.ReadUInt32() + 4;
 
-                br.Close();
+                    if (pos + 2 > br.BaseStream.Length)
+                        return PEType.Unknown;
 
-                if (machine == 0x014C)
-                    return PEType.X32;
+                    br.BaseStream.Seek(pos, SeekOrigin.Begin);
+                    var machine = br.ReadUInt16();
 
-                if (machine == 0x8664)
-                    return PEType.X64;
+                    if (machine == 0x014C)
+                        return PEType.X32;
+                    else if (machine == 0x8664)
+                        return PEType.X64;
 
-                return PEType.Unknown;
+                    return PEType.Unknown;
+                }
             }
             catch
             {
